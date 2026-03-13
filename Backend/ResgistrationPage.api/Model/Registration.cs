2ff3ecfb7184e7d9d3ea050140ee65233ee13f43
Konark1 <konark107@gmail.com>
2026-03-13 16:57:53 +0530

    Initial commit - student registration page

---
+++ Backend/ResgistrationPage.api/Model/Registration.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResgistrationPage.api.Model
+{
+    [Table("studentMaster")]
+    public class Registration
+    {
+        [Key]
+        public int studentId { get; set; }
+
+        [Required, MaxLength(50)]
+        public string studName { get; set; } = string.Empty;
+
+        [Required, MaxLength(10)]
+        public string mobileNo { get; set; } = string.Empty;
+
+        [Required, MaxLength(50)]
+        public string email { get; set; } = string.Empty;
+        
+        [MaxLength(50)]
+        public string city { get; set; } = string.Empty;
+
+        [MaxLength(50)]
+        public string state { get; set; } = string.Empty;
+
+        [MaxLength(6)]
+        public string pincode { get; set; } = string.Empty;
+
+        [MaxLength(2000)]
+        public string addressLine1 { get; set; } = string.Empty;
+
+        [MaxLength(2000)]
+        public string addressLine2 { get; set; } = string.Empty;
+    }
+
+}
+    
---
+++ Backend/ResgistrationPage.api/Model/RegistrationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ResgistrationPage.api.Model
+{
+    public class RegistrationDbContext:DbContext
+    {
+        public RegistrationDbContext(DbContextOptions<RegistrationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Registration> Registrations { get; set; }
+    }
+}
